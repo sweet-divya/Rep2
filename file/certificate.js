b173e5b1c5e83b9f0fd8b0ebd6b6f32ddf129a67
sweet-divya <kartikaaryan2706@gmail.com>
2025-05-10 17:19:24 +0530

    Add files via upload

---
+++ file/certificate.js
@@ -0,0 +1,9 @@
+function redirectToCert(){
+    let button = document.getElementById('certi');
+    window.location.href = 'page1.html'
+}
+
+function redirectTobdayvid(){
+    let button = document.getElementById('bday-vid');
+    window.location.href = 'page2.html'
+}
